34dcd804e3e6bbd0e8a12ceaaeb1bf2bc0b16b29
angelillis.artem <angelillis.artem@studenti.salesianisesto.it>
2023-01-09 12:53:08 +0100

    Second Commit

--- src/Treno.java
+++ src/Treno.java
@@ -1,10 +1,10 @@
 public class Treno {
-    int capienzaVagone = 50;
-    int[] maxCapienzaVagone = new int[capienzaVagone];
+    int capienzaVagone = 10;
     int[] arrayTreno = new int[5];
 
     public void insPasscasuali() {
         for (int i = 0; i < arrayTreno.length; i++)
             arrayTreno[i] = (int) (Math.random() * 5);
     }
+
 }
