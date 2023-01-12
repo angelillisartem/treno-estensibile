860f56d9cd0ca3b27cceaa4f1f5829f767286159
angelillis.artem <angelillis.artem@studenti.salesianisesto.it>
2023-01-12 13:35:13 +0100

    third commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,29 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        Random r = new Random();
 
-        System.out.print("Quante persone ci sono sul treno?: ");
-        int totPersone = in.nextInt();
+        //Genera a random i passegeri che salgono e scendono dal treno
+        int passSaliti = (int) (Math.random() * 20);
+        int passScesi = (int) (Math.random() * 20);
 
+        //
+        //Stampare le fermate del treno
+        System.out.print("a che feramata siamo?: ");
+        String tappa = in.next();
+        //Modificare il numero dei vagoni se il numero massimo di cpienza viene superato
+        //
 
-        // Crea una serie di tappe
-        String[] tappe = { "Piacenza", "Bologna", "Firenze", "Grosseto", "Rieti", "Roma"};
+        Treno treno = new Treno(20, 5, passSaliti, passScesi, tappa);
+        treno.setMaxPass(20);
+        treno.setMaxVagoni(5);
+        treno.setPassSaliti(passSaliti);
+        treno.setPassScesi(passScesi);
+        treno.setTappe(tappa);
 
+        treno.stampa();
     }
 }
--- src/Treno.java
+++ src/Treno.java
@@ -1,10 +1,50 @@
 public class Treno {
-    int capienzaVagone = 10;
-    int[] arrayTreno = new int[5];
-
-    public void insPasscasuali() {
-        for (int i = 0; i < arrayTreno.length; i++)
-            arrayTreno[i] = (int) (Math.random() * 5);
+   private int maxPass;
+   private int maxVagoni;
+   private int passSaliti;
+   private int passScesi;
+   private String tappe;
+    public Treno(int maxPass, int maxVagoni, int passSaliti, int passScesi, String tappe) {
+        this.maxPass = maxPass;
+        this.maxVagoni = maxVagoni;
+        this.passSaliti = passSaliti;
+        this.passScesi = passScesi;
+        this.tappe = tappe;
     }
-
+    public void setMaxPass(int maxPass) {
+        this.maxPass = maxPass;
+    }
+    public void setMaxVagoni(int maxVagoni) {
+        this.maxVagoni = maxVagoni;
+    }
+    public void setPassSaliti(int passSaliti) {
+        this.passSaliti = passSaliti;
+    }
+    public void setPassScesi(int passScesi) {
+        this.passScesi = passScesi;
+    }
+    public void setTappe(String tappe) {
+        this.tappe = tappe;
+    }
+    public String getTappe() {
+        return tappe;
+    }
+    public int getMaxVagoni() {
+        return maxVagoni;
+    }
+    public int getMaxPass() {
+        return maxPass;
+    }
+    public int getPassSaliti() {
+        return passSaliti;
+    }
+    public int getPassScesi() {
+        return passScesi;
+    }
+    public void stampa() {
+        System.out.print("\nIl treno e' alla tappa: " + getTappe());
+        System.out.print("\nMassimo passegeri sul treno: " + getMaxPass());
+        System.out.print("\nPassegeri saliti sul treno: " + getPassSaliti());
+        System.out.print("\nPassegeri scesi dal treno: " + getPassScesi());
+    }
 }
